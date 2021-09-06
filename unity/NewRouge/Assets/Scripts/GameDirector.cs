a95d216aef81bfb34de4443975d9e876079eeb2a
DanielMartinez98 <56524433+DanielMartinez98@users.noreply.github.com>
2021-09-06 02:16:30 -0500

    update, agregamos texto

--- unity/NewRouge/Assets/Scripts/GameDirector.cs
+++ unity/NewRouge/Assets/Scripts/GameDirector.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-
+using TMPro;
 using System.Diagnostics;
  
 public class GameDirector : MonoBehaviour
@@ -12,6 +12,22 @@ public class GameDirector : MonoBehaviour
     public GameObject error;
     public GameObject floor;
     public TextAsset textFile;
+    public Sprite rogue;
+    public Sprite bat;
+    public Sprite emu;
+    public Sprite food;
+    public Sprite gold;
+    public Sprite hobgoblin;
+    public Sprite icemonster;
+    public Sprite kestrel;
+    public Sprite potion;
+    public Sprite scroll;
+    public Sprite snake;
+    public Sprite stairs;
+    public Sprite sword;
+    public Sprite unknown;
+    public TMP_Text texto;
+    public TMP_Text texto2;
     // Start is called before the first frame update
     private void Start()
     {
@@ -31,27 +47,40 @@ public class GameDirector : MonoBehaviour
             string line;
             while ((line = sr.ReadLine()) != null)
             {
-                for (int i = 0; i < line.Length; i++)
-					switch(line[i]) {
-						case ' ':
-						break;
-						
-						case '.':
-							spawn(floor, (float)i, (float)0.0, (float)count);
-						break;
-						
-						case '|':
-						case '-':
-							spawn(paredes, (float)i, (float)0.5, (float)count);
-						break;
-						
-						default:
-							GameObject a = spawn(player, (float)i, (float)0.0, (float)count);
-							a.GetComponent<Sprite>(getType(line[i]));
-							spawn(floor, (float)i, (float)0.0, (float)count);
-						break;
-					}
-				
+                if (count == 0)
+                {
+                    texto.text = line;
+                } 
+                else if(count ==23)
+                {
+                texto2.text = line;
+                }
+                else
+                {
+                    for (int i = 0; i < line.Length; i++)
+                        switch (line[i])
+                        {
+                            case ' ':
+                                break;
+
+                            case '.':
+                            case '#':
+                            case '+':
+                                spawn(floor, (float)i, (float)0.0, (float)count);
+                                break;
+
+                            case '|':
+                            case '-':
+                                spawn(paredes, (float)i, (float)0.5, (float)count);
+                                break;
+
+                            default:
+                                GameObject a = spawn(player, (float)i, (float)0.0, (float)count);
+                                a.GetComponent<SpriteRenderer>().sprite = getType(line[i]);
+                                spawn(floor, (float)i, (float)0.0, (float)count);
+                                break;
+                        }
+                }
                 count++;
             }
         }
@@ -61,21 +90,31 @@ public class GameDirector : MonoBehaviour
 		GameObject a = Instantiate(obj);
         a.transform.parent = transform;
         a.transform.localPosition = new Vector3(_x, _y, _z);
+        return a;
 	}
 
     Sprite getType(char ch)
     {
-		string path = "Assets/Sprites/";
-		string ext = ".png";
-		string spr;
+		
 		
         switch (ch)
         {
-            case '@':	spr = "rogue";		break;
-			default:	spr = "unknown";	break;
+            case '@': return rogue;	break;
+            case 'B': return bat; break;
+            case 'E': return emu; break;
+            case 'H': return hobgoblin; break;
+            case 'I': return icemonster; break;
+            case 'K': return kestrel; break;
+            case 'S': return snake; break;
+            case '?': return scroll; break;
+            case '*': return gold; break;
+            case ':': return food; break;
+            case '!': return potion; break;
+            case ')': return sword; break;
+            case '%': return stairs; break;
+            default: return unknown; break;
         }
 		
-		return Resources.Load<Sprite>(path+spr+ext);
     }
 
     // Update is called once 
